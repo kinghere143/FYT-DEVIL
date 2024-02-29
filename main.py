aabc1295814ff1ce955a339d593e1d38c55ff098
kinghere143 <160225072+kinghere143@users.noreply.github.com>
2024-03-01 00:40:41 +0530

    Update and rename streamlit_app.py to main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
           self.send_response(200)
           self.send_header('Content-type', 'text/plain')
           self.end_headers()
-          self.wfile.write(b"-- SERVER RUNNING>>ZAIN MALIK HERW")
+          self.wfile.write(b"-- SERVER RUNNING>>ZAIN MALIK HEW")
 def execute_server():
       PORT = 4000
 
